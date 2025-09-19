45097e9a41bd4539df3841b12db7c74a3d3d197e
thisishappy <1379This_is_HappyEL@mail.ru>
2025-09-19 15:06:33 +0700

    Правлю ошибки

--- brain_games/scripts/rounds_logic.py
+++ brain_games/scripts/rounds_logic.py
@@ -8,7 +8,7 @@ def rounds_logic(generate_game_stage, game_description):
     for _ in range(3):
         question, correct_answer = generate_game_stage()
 
-        print(f'Question {question}')
+        print(f'Question: {question}')
         player_answer = prompt.string('Your answer: ')
 
         if (player_answer == str(correct_answer)):
